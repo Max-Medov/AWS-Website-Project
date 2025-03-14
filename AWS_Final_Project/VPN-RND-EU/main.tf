557622b87e8544ada5e5ecfdbc5b164b369af517
Max-Medov <max19900@gmail.com>
2025-03-14 12:41:30 +0200

    fixed main vpn

--- AWS_Final_Project/VPN-RND-EU/main.tf
+++ AWS_Final_Project/VPN-RND-EU/main.tf
@@ -8,14 +8,6 @@ data "aws_vpc" "existing_vpc" {
   tags = { Name = var.vpc_name_tag }
 }
 
-# Existing private route table explicitly referenced
-data "aws_route_table" "private_rt_eu" {
-  filter {
-    name   = "tag:Name"
-    values = [var.private_rt_tag]
-  }
-}
-
 resource "aws_vpn_gateway" "rnd_vgw" {
   vpc_id = data.aws_vpc.existing_vpc.id
   tags   = { Name = "RND-VGW-EU" }
@@ -34,12 +26,5 @@ resource "aws_vpn_connection" "vpn_conn" {
   type                = "ipsec.1"
   static_routes_only  = false
   tags                = { Name = "RND-to-IT-VPN" }
-}
-
-# Automatically create route explicitly to IT account
-resource "aws_route" "vpn_route_to_it" {
-  route_table_id         = data.aws_route_table.private_rt_eu.id
-  destination_cidr_block = var.it_cidr_block
-  gateway_id             = aws_vpn_gateway.rnd_vgw.id
 }
 
